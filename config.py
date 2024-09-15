ffecc9924d2848bc63ea431b823e7ad2bdeb7557
SihagBots <sihagamit308@gmail.com>
2024-09-15 20:31:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -34,15 +34,15 @@ def is_enabled(value, default):
 # Please Love & Support 💗💗🙏
       
 # Owner Information
-API_ID = int(environ.get("API_ID", "26626715"))
-API_HASH = environ.get("API_HASH", "967f1c73aa77a29009bc364edd30b525")
-ADMINS = int(environ.get("ADMINS", "7216492043"))
+API_ID = int(environ.get("API_ID", "29346781"))
+API_HASH = environ.get("API_HASH", "75fb004873db1864a09c71cd1307bfa8")
+ADMINS = int(environ.get("ADMINS", "5860332990"))
 
 # Database Information
-CLONE_DB_URI = environ.get("CLONE_DB_URI", "mongodb+srv://cdbpandafile:cdbpandafile@cdbpandafile.vmtpr6u.mongodb.net/?retryWrites=true&w=majority&appName=CDBPandaFile")
-CDB_NAME = environ.get("CDB_NAME", "CDBPandaFile")
-DB_URI = environ.get("DB_URI", "mongodb+srv://dbpandafile:dbpandafile@dbpanda.jzibnmg.mongodb.net/?retryWrites=true&w=majority&appName=DBPanda")
-DB_NAME = environ.get("DB_NAME", "DBPanda")
+CLONE_DB_URI = environ.get("CLONE_DB_URI", "mongodb+srv://Sihagbots:585xpplus@cluster0.7o52a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+CDB_NAME = environ.get("CDB_NAME", "Cluster0")
+DB_URI = environ.get("DB_URI", "mongodb+srv://Sihagbots:585xpplus@cluster0.7o52a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = environ.get("DB_NAME", "Cluster0")
 
 # Credit @TheBlackXYZ.
 # Please Don't remove credit.
@@ -53,17 +53,17 @@ DB_NAME = environ.get("DB_NAME", "DBPanda")
 # Please Love & Support 💗💗🙏
 
 # Bot Information
-BOT_TOKEN = environ.get("BOT_TOKEN", "6751281414:AAHD82JheC-ujDJFarjqy0yd633c7uOPPfQ")
-BOT_USERNAME = environ.get("BOT_USERNAME", "panda_files_bot") # your bot username without @
+BOT_TOKEN = environ.get("BOT_TOKEN", "6811483504:AAGL9kHd8m18foMwtyCnPFRhgLihREPbRk4")
+BOT_USERNAME = environ.get("BOT_USERNAME", "PrivatefilestoreRoBot") # your bot username without @
 PICS = (environ.get('PICS', 'https://telegra.ph/file/84b09cfc6223afcd75393.jpg')).split() # Bot Start Picture
 
 # Auto Delete Information
-AUTO_DELETE = int(environ.get("AUTO_DELETE", "5")) # Time in Minutes
-AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "300")) # Time in Seconds
+AUTO_DELETE = int(environ.get("AUTO_DELETE", "1")) # Time in Minutes
+AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "60")) # Time in Seconds
 
 # Channel Information
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002187486541"))
-FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-1002051659577')).split()]
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002386658352"))
+FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-1002311825454')).split()]
 
 # Credit @TheBlackXYZ.
 # Please Don't remove credit.
@@ -95,7 +95,7 @@ class Var(object):
     name = str(getenv('name', 'theblackfilestore'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002197998836'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001948204364'))
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
